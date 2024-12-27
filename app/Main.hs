2c3595b4f62d1b0f5d031811424c7c6c0cff5122
Jack <jackrosenberg03@gmail.com>
2024-12-27 19:24:06 +0100

    added front

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import Prelude as P
 import Control.Lens as C
 import Graphics.Image as I
 import Data.Maybe (fromMaybe)
-import Data.List (findIndex)
+import Data.List (findIndex, nub)
 import Debug.Trace (trace)
 import Control.Monad (liftM2)
 import System.Random
@@ -35,7 +35,7 @@ between :: (Ord a) => a -> a -> a -> Bool
 between l x u = x >= l && x <= u 
 
 defaultTile, horizTile, vertTile, crossTile :: Tile -- make the tiles
-defaultTile = tileGen 90 (const (PixelRGB 0 0 0))
+defaultTile = tileGen 90 (const (PixelRGB 1 0 0))
 horizTile = tileGen 90 (\(x, _) -> if between (90`div`3) x (2*90`div`3) then PixelRGB 255 255 255 else PixelRGB 0 0 0)
 vertTile  = tileGen 90 (\(_, y) -> if between (90`div`3) y (2*90`div`3) then PixelRGB 255 255 255 else PixelRGB 0 0 0)
 crossTile = tileGen 90 (\(x, y) -> if between (90`div`3) x (2*90`div`3) || between (90`div`3) y (2*90`div`3) then PixelRGB 255 255 255 else PixelRGB 0 0 0)
@@ -51,7 +51,7 @@ tilelist = [horizTile ,vertTile, crossTile]
 main :: IO ()
 main = do 
     g <- getStdGen
-    let res = wfc g (genGrd 20) cst 
+    let res = wfc g cst (genGrd 10) 
     let wfcres = combineTiles $ toTiles res
     print $ length <$> concat res
     write ("res",  wfcres)
@@ -69,23 +69,24 @@ write (name, tile) = writeImage ("images/" ++ name ++ ".png") (view image tile)
 
 
 combineTiles :: [Tile] -> Tile
-combineTiles tl = add tl canvas 0
-    where canvas = tileGen (cols((view image . head) tl) * width) (\(_,_) -> PixelRGB 0 0 0) 
+combineTiles tl = Tile $ foldr (\(im, (r,c)) acc -> superimpose (r*rows im, c*cols im) im acc) canvas imsWidx
+    where imsWidx = P.zip (_image <$> tl) [(r, c) | r <- [0..(width -1)], c <- [0..(width -1)]] 
           width  = (ceiling . sqrt . fromIntegral . length) tl
-          add :: [Tile] -> Tile -> Int -> Tile
-          add [] acc  _    = acc
-          add (t:ts) acc i = add ts (Tile (superimpose ((i `div` width)*cols (view image t),(i `mod` width)*cols (view image t)) (view image t) (view image acc))) (i+1)
-
+          canvas = canvasSize Wrap (incBy (width, width)) di
+          di =  defaultTile^.image
+          incBy (fm, fn) = (rows di * fm, cols di * fn)
 
 cst :: [Constraint Tile]
 cst = [noAdj]
 
-c12, noAdj :: (Int, Int) -> (Int, Int) -> Tile -> Tile -> Bool -- takes two indexes and gives func to compare
+c12, noAdj, cross :: Constraint Tile
 
 c12 (0,0) (0,1) = (==)
 c12 _ _ = const $ const False
 
-noAdj _ _ = (==) -- universal constraint??
+cross (r1,c1) (r2,c2) t1 t2 = r1 == r2 && c1 == c2+1 && t1 == crossTile && t2 /= horizTile
+
+noAdj _ _ = (/=) -- universal constraint??
 
 genGrd :: Int -> Grid (Domain Tile)
 genGrd sz = replicate sz (replicate sz tilelist) -- domains start all possibilities
@@ -103,21 +104,27 @@ non1min = foldr (\i acc -> if i <= 1 then acc else min i acc) 9999
 pop :: (RandomGen g) => g -> Domain Tile -> (Domain Tile, g) 
 pop g d = let (i, ng) = randomR (0, length d -1) g in ([d!!i], ng)
 
-succs :: (Int, Int) -> Grid a -> [(Int, Int)] -- get succs of a list
-succs (r,c) g = [(nr, nc) | nr <- [r-1, r, r+1], between 0 nr (length . head $ g),
+succs :: Grid a -> (Int, Int) -> [(Int, Int)] -- get succs of a list
+succs g (r,c)  = [(nr, nc) | nr <- [r-1, r, r+1], between 0 nr (length . head $ g),
                             nc <- [c-1, c, c+1], between 0 nc (length . head $ g), 
                            (nr == r) /= (nc == c)] --xor bitch
 
-wfc :: RandomGen g => g -> Grid (Domain Tile) -> [Constraint Tile] -> Grid (Domain Tile)
-wfc g inp cs | all (all ((<= 1) . length)) inp = inp
-             | otherwise                       = wfc ng (collapse (over (idx n) (const nd) inp) cs n (succs n inp)) cs
+wfc :: RandomGen g => g -> [Constraint Tile] -> Grid (Domain Tile) -> Grid (Domain Tile)
+wfc g cs inp = let (g, f) = collapse (over (idx n) (const nd) inp) cs n in trace ("front: " ++ show f) g
     where (nd, ng) = pop g (inp !!! n) 
           n = minDex inp
 
-collapse :: Grid (Domain Tile) -> [Constraint Tile] -> (Int, Int) -> [(Int, Int)] -> Grid (Domain Tile)
-collapse inp cs n = foldr (liftM2 over idx prune) inp -- fold over the array and prune forall successors
-    where prune :: (Int, Int) -> Domain Tile -> Domain Tile -- remove vals in target that violate constraint forall values 
-          prune s d = foldr (\csr -> filter (\e -> none (csr n s e) (inp !!! n))) d cs
+collapse :: Grid (Domain Tile) -> [Constraint Tile] -> (Int, Int) -> (Grid (Domain Tile), [(Int, Int)])
+collapse inp cs n = foldr (\s (g,f) -> (over (idx s) (snd . prune s) g, if fst $ prune s (inp !!!n) then nub $ succs g s ++ f else f)) (inp, []) (succs inp n) -- fold over the array and prune forall successors
+    where prune :: (Int, Int) -> Domain Tile -> (Bool, Domain Tile) -- remove vals in target that violate constraint forall values and if they changed
+          prune s d = let res = foldr (\csr -> filter (\e -> none (csr n s e) (inp !!! n))) d cs in (res == d, res)
 
 idx :: (Int, Int) -> ASetter (Grid (Domain Tile)) (Grid (Domain Tile)) (Domain Tile) (Domain Tile)
 idx s = element (fst s) . element (snd s)
+
+
+again :: Int -> (a->a) -> a -> a
+again 1 f a = f a 
+again n f a = f $ again (n-1) f a
+
+
