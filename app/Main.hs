38bbe5358be1fbc20b9272954b4dd563134efcc0
Jack <jackrosenberg@gmail.com>
2025-02-05 19:02:03 +0100

    she works

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ import System.Random
 import Graphics.Gloss
 import GHC.Float (int2Float)
 import qualified Data.Set as S
-import Graphics.Gloss.Interface.IO.Game (Key (SpecialKey), Event (EventKey), KeyState (Down, Up), SpecialKey (KeySpace))
+import Graphics.Gloss.Interface.IO.Game (Key (SpecialKey), Event (EventKey), KeyState (Down, Up), SpecialKey (KeySpace, KeyEnter), playIO)
 
 data World = World
     { _keys :: S.Set Key
@@ -35,66 +35,74 @@ $(makeLenses ''World)
 between :: (Ord a) => a -> a -> a -> Bool
 between l x u = x >= l && x <= u 
 
-
 horizTile ,vertTile, crossTile, collapseTile, defaultTile :: Tile
-horizTile = Tile { _image = pictures [color black $ rectangleSolid 90 90, color white $ rectangleSolid 90 30] }
-vertTile  = Tile { _image = pictures [color black $ rectangleSolid 90 90, color white $ rectangleSolid 30 90] }
-crossTile = Tile { _image = pictures [color black $ rectangleSolid 90 90, color white $ rectangleSolid 30 90, color white $ rectangleSolid 90 30]}
+horizTile = Tile { _image = pictures [color black $ rectangleSolid tileSz tileSz, color white $ rectangleSolid tileSz (tileSz/3)] }
+vertTile  = Tile { _image = pictures [color black $ rectangleSolid tileSz tileSz, color white $ rectangleSolid (tileSz/3) tileSz] }
+crossTile = Tile { _image = pictures [color black $ rectangleSolid tileSz tileSz, color white $ rectangleSolid (tileSz/3) tileSz, color white $ rectangleSolid tileSz (tileSz/3)]}
 
-collapseTile = Tile { _image = color red $ rectangleSolid 90 90   }
-defaultTile  = Tile { _image = color black $ rectangleSolid 90 90 }
-
-
-handleInput :: Event -> World -> World
-handleInput (EventKey k Down _ _) env = env { _keys = S.insert k (_keys env)}
-handleInput (EventKey k Up _ _)   env = env { _keys = S.delete k (_keys env)}
-handleInput _ env = env -- Ignore non-keypresses for simplicity
-
-update :: RandomGen g => g -> Float -> World -> World
-update g _ world
-    | S.member (SpecialKey KeySpace) (_keys world) = over grid (const res) world
-    | otherwise = world
-    where (res, _) = wfco ng cst (world^.grid)
-          (ng, _) = split g
+collapseTile = Tile { _image = color red $ rectangleSolid tileSz tileSz   }
+defaultTile  = Tile { _image = color black $ rectangleSolid tileSz tileSz }
 
 tilelist :: [Tile]
 tilelist = [horizTile ,vertTile, crossTile]
 
-render :: World -> Picture
-render w = translate (-(45*int2Float (sz-1))) (-(45*int2Float (sz-1))) $ pictures b
+handleInput :: Event -> World -> IO World
+handleInput (EventKey k Down _ _) env = pure $ env { _keys = S.insert k (_keys env)}
+handleInput (EventKey k Up   _ _) env = pure $ env { _keys = S.delete k (_keys env)}
+handleInput _                     env = pure env -- Ignore non-keypresses for simplicity
+
+update :: Float -> World -> IO World
+update _ world
+    | S.member (SpecialKey KeySpace) (_keys world) = 
+        do 
+            g <- newStdGen -- NEEDS TO BE NEW FUCK YES I FOUND IT
+            let res = wfco g cst (world^.grid)
+            pure $ over grid (const res) world
+    |  S.member (SpecialKey KeyEnter) (_keys world) = pure emptyWrld 
+    | otherwise = pure world
+
+render :: World -> IO Picture
+render w = pure $ translate (-((tileSz/2)*int2Float (numTiles-1))) (-((tileSz/2)*int2Float (numTiles-1))) $ pictures b
     where 
      res = w^.grid  
-     l = zip [(r, c) | r <- [0..(sz -1)], c <- [0..(sz -1)]] (concat res)
+     l = zip [(r, c) | r <- [0..(numTiles -1)], c <- [0..(numTiles -1)]] (concat res)
      fr = minDeces res
-     b = (\((r, c), d) -> let col = if (r,c) `elem` fr then green else light orange in translate (int2Float r*90) (int2Float c*90) (toTl d col ^. image)) <$> l 
+     b = (\((r, c), d) -> let col = if (r,c) `elem` fr then green else light orange in translate (int2Float r*tileSz) (int2Float c*tileSz) (toTl d col ^. image)) <$> l 
 
+-- magic constants
 ------------------------------------------------------
-sz = 9
-window = InWindow "" (90*sz, 90*sz) (0, 0)
+screenSz = (1400, 1400) 
+numTiles = 9
+tileSz = int2Float $ fst screenSz `div` numTiles :: Float
+window = InWindow "" screenSz (0, 0)
 background = light black
-cst = [noAdj]   :: [Constraint Tile]
-grd = genGrd sz :: Grid (Domain Tile)
-------------------------------------------------------
-
-
+cst = [noAdj]         :: [Constraint Tile]
+grd = genGrd numTiles :: Grid (Domain Tile)
+emptyWrld = World {_keys = S.empty, _grid = grd}
 main :: IO ()
 main = do
-    g <- getStdGen
-    play window background 30 (World {_keys = S.empty , _grid = grd}) render handleInput (update g)
-    
+    playIO 
+        window
+        background
+        30
+        emptyWrld 
+        render
+        handleInput
+        update 
+------------------------------------------------------
 
+-- display tile or amount of options in domain
 toTl :: Domain Tile -> Color -> Tile
-toTl [] _= trace "dumfuk, wave function collapsed" collapseTile
+toTl [] _  = trace "dumfuk, wave function collapsed" collapseTile
 toTl [i] _ = i
-toTl l@(i:is) c = Tile {_image = (scale 0.5 0.5 . translate (-45) (-45) . color c . text . show . length) l}
+toTl l c   = Tile {_image = (scale 0.5 0.5 . translate (-45) (-45) . color c . text . show . length) l}
+
 noAdj, cross :: Constraint Tile
-
 cross (r1,c1) (r2,c2) t1 t2 =  r1 == r2 && (t1 /= crossTile) && (t2 /= horizTile)
-
 noAdj _ _ = (==) -- universal constraint??
 
 genGrd :: Int -> Grid (Domain Tile)
-genGrd sz = replicate sz (replicate sz tilelist) -- domains start all possibilitiesidcs
+genGrd sz = replicate sz (replicate sz tilelist) -- domains start all possibilities
 
 minDeces :: Grid (Domain Tile) -> [(Int, Int)] -- (row,col)
 minDeces g = (\ai -> (ai `div` f, ai `mod` f)) <$> ids
@@ -107,7 +115,7 @@ non1min :: [Int] -> Int
 non1min = foldr (\i acc -> if i <= 1 then acc else min i acc) 9999
 
 pop :: (RandomGen g) => g -> Domain Tile -> (Domain Tile, g) 
-pop g d = trace ("i: " <> show i <> " ld: " <> show (length d-1)) ([d!!i], ng)
+pop g d = ([d!!i], ng) --trace ("i: " <> show i <> " ld: " <> show (length d-1)) 
         where (i, ng) = randomR (0, length d-1) g
  
 succs :: Grid a -> (Int, Int) -> [(Int, Int)] -- get succs of a list
@@ -123,9 +131,9 @@ done  = all $ all $ (<=1) . length
 
 -- Wfc once:
 -- top level function, pops a random tile with the least entropy and calls collapse on it
-wfco :: RandomGen g => g -> [Constraint Tile] -> Grid (Domain Tile) -> (Grid (Domain Tile), g)
-wfco g cs inp | done inp  = (inp, g)
-              | otherwise = let r = fst $ collapse cs (over (idx n) (const nd) inp, succs inp n) in (r, nnng)
+wfco :: RandomGen g => g -> [Constraint Tile] -> Grid (Domain Tile) -> Grid (Domain Tile)
+wfco g cs inp | done inp  = inp
+              | otherwise = fst $ collapse cs (over (idx n) (const nd) inp, succs inp n) 
     where (nd, nnng) = pop ng (inp !!! n) 
           idcs = minDeces inp
           (n, ng) = let (ni, ng) = randomR (0, length idcs -1) g in (idcs !! ni, ng)
